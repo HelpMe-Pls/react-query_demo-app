8f634eab26d08493703b6baf87f7a1c04322203d
Random Guy <leekhoi5319990@gmail.com>
2022-03-09 17:08:04 +0700

    Centralized Loading & Error

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -3,16 +3,15 @@ import { useQuery } from 'react-query'
 import type { Treatment } from '../../../../../shared/types'
 import { axiosInstance } from '../../../axiosInstance'
 import { queryKeys } from '../../../react-query/constants'
-import { useCustomToast } from '../../app/hooks/useCustomToast'
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
-  const { data } = await axiosInstance.get('/treatments')
-  return data
+	const { data } = await axiosInstance.get('/treatments')
+	return data
 }
 
 export function useTreatments(): Treatment[] {
-  const fallback = [] // to prevent error when there's no "data" yet
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments)
-  return []
+	const fallback = [] // to prevent error when there's no "data" yet
+	const { data = fallback } = useQuery(queryKeys.treatments, getTreatments)
+	return data
 }
