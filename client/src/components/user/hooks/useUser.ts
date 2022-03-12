c2ef5e3d28e82bd8009f74b09d45db00eb4c1331
Random Guy <leekhoi5319990@gmail.com>
2022-03-12 17:14:17 +0700

    .

--- client/src/components/user/hooks/useUser.ts
+++ client/src/components/user/hooks/useUser.ts
@@ -4,51 +4,51 @@ import type { User } from '../../../../../shared/types'
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance'
 import { queryKeys } from '../../../react-query/constants'
 import {
-  clearStoredUser,
-  getStoredUser,
-  setStoredUser,
+	clearStoredUser,
+	getStoredUser,
+	setStoredUser
 } from '../../../user-storage'
 
 async function getUser(user: User | null): Promise<User | null> {
-  if (!user) return null
-  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-    `/user/${user.id}`,
-    {
-      headers: getJWTHeader(user),
-    }
-  )
-  return data.user
+	if (!user) return null
+	const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+		`/user/${user.id}`,
+		{
+			headers: getJWTHeader(user),
+		}
+	)
+	return data.user
 }
 
 interface UseUser {
-  user: User | null
-  updateUser: (user: User) => void
-  clearUser: () => void
+	user: User | null
+	updateUser: (user: User) => void
+	clearUser: () => void
 }
 
 export function useUser(): UseUser {
-  const queryClient = useQueryClient()
-  const { data: user } = useQuery(queryKeys.user, () => getUser(user), {
-    initialData: getStoredUser(), // so that when the user REFRESHES the page after they logged in, they're still logged in (by the data that is persisted in localStorage from the onSuccess option below)
-    onSuccess: (received: User | null) => {
-      if (!received) clearStoredUser()
-      // null: user logged out
-      else setStoredUser(received)
-    },
-  })
+	const queryClient = useQueryClient()
+	const { data: user } = useQuery(queryKeys.user, () => getUser(user), {
+		initialData: getStoredUser(), // so that when the user REFRESHES the page after they logged in, they're still logged in (by the data that is persisted in localStorage from the onSuccess option below)
+		onSuccess: (received: User | null) => {
+			if (!received) clearStoredUser()
+			// null: user logged out
+			else setStoredUser(received)
+		},
+	})
 
-  // update the user in the query cache
-  function updateUser(newUser: User): void {
-    queryClient.setQueryData(queryKeys.user, newUser)
-  }
+	// update the user in the query cache
+	function updateUser(newUser: User): void {
+		queryClient.setQueryData(queryKeys.user, newUser)
+	}
 
-  // meant to be called from useAuth
-  function clearUser() {
-    queryClient.removeQueries([queryKeys.appointments, queryKeys.user])
+	// meant to be called from useAuth
+	function clearUser() {
+		queryClient.removeQueries([queryKeys.appointments, queryKeys.user])
 
-    // not using removeQueries(queryKeys.user) because it won't trigger the {onSuccess}
-    queryClient.setQueryData(queryKeys.user, null)
-  }
+		// not using removeQueries(queryKeys.user) because it won't trigger the {onSuccess}
+		queryClient.setQueryData(queryKeys.user, null)
+	}
 
-  return { user, updateUser, clearUser }
+	return { user, updateUser, clearUser }
 }
