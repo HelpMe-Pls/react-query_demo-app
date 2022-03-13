f0288ef553d99bf91577d4b9af1539e752cb0c96
Random Guy <leekhoi5319990@gmail.com>
2022-03-13 17:05:17 +0700

    Setting up Tests

--- client/src/components/treatments/tests/Treatments.test.tsx
+++ client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,7 @@
-import { screen } from '@testing-library/react';
-
-import { Treatments } from '../Treatments';
+import { screen } from '@testing-library/react'
+import { Treatments } from '../Treatments'
+import { renderWithQueryClient } from '../../../test-utils'
 
 test('renders response from query', () => {
-  // write test here
-});
+	renderWithQueryClient(<Treatments />)
+})
