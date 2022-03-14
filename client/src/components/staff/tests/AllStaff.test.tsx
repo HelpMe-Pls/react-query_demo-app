edb721ca1169e2fad5bb10a403c1b40deb5df749
Random Guy <leekhoi5319990@gmail.com>
2022-03-14 18:02:15 +0700

    Testing mutations

--- client/src/components/staff/tests/AllStaff.test.tsx
+++ client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,8 +1,7 @@
 import { screen } from '@testing-library/react'
 
-// import { rest } from 'msw';
-// import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { server } from '../../../mocks/server';
+import { rest } from 'msw'
+import { server } from '../../../mocks/server'
 import { renderWithQueryClient } from '../../../test-utils'
 import { AllStaff } from '../AllStaff'
 
@@ -16,11 +15,16 @@ test('renders response from query', async () => {
 	expect(staffTitles).toHaveLength(4)
 })
 
-// test('handles query error', async () => {
-// (re)set handler to return a 500 error for staff
-// server.resetHandlers(
-//   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-//     return res(ctx.status(500));
-//   }),
-// );
-// })
+test('handles query error', async () => {
+	// (re)set handler to return a 500 error for staff
+	server.resetHandlers(
+		rest.get('http://localhost:3030/staff', (_req, res, ctx) => {
+			return res(ctx.status(500))
+		})
+	)
+
+	renderWithQueryClient(<AllStaff />)
+
+	const alertToast = await screen.findByRole('alert')
+	expect(alertToast).toHaveTextContent('Request failed with status code 500')
+})
